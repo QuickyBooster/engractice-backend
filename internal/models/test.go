125e9e0dc8a63607fdc9019b45a30ef0167cf616
QuickyBooster <quickybooster@gmail.com>
2025-04-26 15:25:59 +0700

    feat: create test and finish test

--- internal/models/test.go
+++ internal/models/test.go
@@ -2,12 +2,15 @@ package models
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Test struct {
-	ID      string       `json:"id" bson:"_id"`
-	Words   []Vocabulary `json:"words"`
-	Date    time.Time    `json:"date"`
-	Correct []Vocabulary `json:"correct"`
-	Wrong   []Vocabulary `json:"wrong"`
+	ID          primitive.ObjectID `bson:"_id" json:"id"`
+	Words       []Vocabulary       `json:"words"`
+	Date        time.Time          `json:"date"`
+	Correct     []Vocabulary       `json:"correct"`
+	Wrong       []Vocabulary       `json:"wrong"`
+	NearestMode bool               `json:"nearestMode"`
 }
